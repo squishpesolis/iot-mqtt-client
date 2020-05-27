29b5cc7fe5cc4a037994059cbcded8a6eb3ee37c
StalynQuishpe <stalin.ct@hotmail.com>
2020-05-27 16:10:38 -0500

    Inicio

---
+++ iot-mqtt-client/iot-mqtt-client/src/main/java/com/commitsolumqttclient/iotmqttclient/IotMqttClientApplication.java
@@ -0,0 +1,13 @@
+package com.commitsolumqttclient.iotmqttclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMqttClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotMqttClientApplication.class, args);
+	}
+
+}
---
+++ iot-mqtt-client/iot-mqtt-client/src/test/java/com/commitsolumqttclient/iotmqttclient/IotMqttClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commitsolumqttclient.iotmqttclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotMqttClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
